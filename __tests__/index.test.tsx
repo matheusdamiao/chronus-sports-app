fe8b6fec3f8ca7f8a8f3046405a8d7422a1d83d8
Matheus Oliveira Damião <84056783+matheusdamiao@users.noreply.github.com>
2024-04-10 17:35:58 -0300

    Theme design system (#1)
    * feat: add design sytems properties to tailwind theme config
    
    * update readme file

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -9,7 +9,7 @@ describe("Home", () => {
     render(<Home />);
 
     const heading = screen.getByRole("heading", {
-      name: 'Chronus Sports',
+      name: 'Welcome to the Largest Sports Platform in the World!',
     });
 
     expect(heading).toBeInTheDocument();
