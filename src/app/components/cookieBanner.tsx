56ef32ad3ebd65af909b3b89c99b47e2c0d6d84e
Matheus Damião <matheus.damiaoliveira@gmail.com>
2024-05-02 15:05:04 -0300

    edit cookie banner position

--- src/app/components/cookieBanner.tsx
+++ src/app/components/cookieBanner.tsx
@@ -6,7 +6,7 @@ const CookieBanner = () => {
   return (
     <>
         <Script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="30b731bc-e9a8-45a0-876c-abb51bdb9480" data-blockingmode="auto" type="text/javascript"></Script>
-        <Script id="CookieDeclaration" src="https://consent.cookiebot.com/30b731bc-e9a8-45a0-876c-abb51bdb9480/cd.js" type="text/javascript" async></Script>
+        {/* <Script id="CookieDeclaration" src="https://consent.cookiebot.com/30b731bc-e9a8-45a0-876c-abb51bdb9480/cd.js" type="text/javascript" async></Script> */}
     </>
   )
 }
